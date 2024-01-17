3254faa187b206329580fabadbbc9579d4193807
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-18 02:07:44 +0500

    add CustomImageProps

--- src/interfaces/props.ts
+++ src/interfaces/props.ts
@@ -1,5 +1,14 @@
 import { ReactNode } from "react";
+import { IProduct } from ".";
 
 export interface IRootLayoutProps {
   children: ReactNode;
+}
+
+export interface CustomImageProps {
+  fill?: boolean;
+  width?: number;
+  height?: number;
+  image: string;
+  title: string;
 }
