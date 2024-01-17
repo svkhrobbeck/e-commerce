a9432934c077a4fd7fc5b21f5a3db540bd06aeb0
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-18 00:17:47 +0500

    add interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,17 @@
+export type INavLink = {
+  route: string;
+  text: string;
+};
+
+export type IProduct = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
