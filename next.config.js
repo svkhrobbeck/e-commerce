95e137689d4ba9c35f01024bb053ef395c1fd75e
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-18 02:07:29 +0500

    optimize

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fakestoreapi.com",
-        pathname: "/img/**",
-      },
-    ],
+    domains: ["fakestoreapi.com"],
   },
 };
 
