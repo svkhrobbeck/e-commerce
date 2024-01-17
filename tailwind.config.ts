10fd0205fe72d654f702dac8f36f5ba8a94ee5d6
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-18 00:16:39 +0500

    configure container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    container: {
+      center: true,
+      padding: "20px",
+    },
   },
   plugins: [],
 };
