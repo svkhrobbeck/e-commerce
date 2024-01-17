191ebbe292d5f5357fe7e68f2737720a3902ba53
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-18 00:16:59 +0500

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+import { INavLink } from "@/interfaces";
+
+export const navLinks: INavLink[] = [
+  {
+    route: "/",
+    text: "Home",
+  },
+  {
+    route: "/products",
+    text: "All Products",
+  },
+];
+
+export const API_BASE_URL = "https://fakestoreapi.com";
+export const API_ENDPOINTS = {
+  PRODUCTS: "/products",
+  PRODUCT: "/products/",
+  CATEGORIES: "/products/categories",
+  CATEGORY: "/products/category/",
+};
