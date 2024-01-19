107c4562f94e8f9ff0631e52c1fbfc1b140884c3
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-19 23:40:19 +0500

    integrate customAxios

--- src/service/products.ts
+++ src/service/products.ts
@@ -1,13 +1,14 @@
 import { IProduct } from "@/interfaces";
 import axios from "axios";
+import customAxios from "./axios";
 
 const ProductsApi = {
   async get(endpoint: string, params: any = {}) {
-    const { data } = await axios.get(endpoint, { params });
+    const { data } = await customAxios.get(endpoint, { params });
     return data as IProduct[];
   },
-  async getOne(endpoint: string, id: number, params: any = {}) {
-    const { data } = await axios.get(endpoint + id, { params });
+  async getOne(endpoint: string, id: number | string, params: any = {}) {
+    const { data } = await customAxios.get(endpoint + id, { params });
     return data as IProduct;
   },
 };
