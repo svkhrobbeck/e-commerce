bf831c8e0cec26bad4998f70ee37c5fb3b1c9287
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-19 23:40:10 +0500

    create customAxios

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,4 +1,8 @@
 import { API_BASE_URL } from "@/constants";
 import axios from "axios";
 
-axios.defaults.baseURL = API_BASE_URL;
+const customAxios = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export default customAxios;
