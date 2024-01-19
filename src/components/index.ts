21b8b15203e3a3721c1f689737865130d4cb5ed8
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-20 00:10:40 +0500

    add cta and feature components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Footer } from "./footer";
 export { default as Product } from "./product";
 export { default as Hero } from "./hero";
 export { default as CustomImage } from "./custom-image";
+export { default as Feature } from "./feature";
+export { default as Cta } from "./cta";
