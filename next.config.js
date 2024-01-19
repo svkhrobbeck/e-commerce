040eb13e95c9929f2920778917a132caec39c6d7
svkhrobbeck <svkhrobbeck@gmail.com>
2024-01-19 23:37:42 +0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fakestoreapi.com"],
+    remotePatterns: [
+      {
+        hostname: "fakestoreapi.com",
+      },
+    ],
   },
 };
 
