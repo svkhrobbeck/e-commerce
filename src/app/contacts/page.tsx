f1b4586a6dd38df89d0a18d671d8314766e089cb
svkhrobbeck <svkhrobbeck@gmail.com>
2024-02-24 13:37:47 +0500

    fix

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -26,17 +26,14 @@ const ContactsPage = () => {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                Photo booth tattooed prism, portland taiyaki hoodie neutra
-                typewriter
+                Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-500 leading-relaxed">
-                example@email.com
-              </a>
+              <a className="text-indigo-500 leading-relaxed">example@email.com</a>
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
                 PHONE
               </h2>
@@ -45,9 +42,7 @@ const ContactsPage = () => {
           </div>
         </div>
         <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-            Feedback
-          </h2>
+          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
           <p className="leading-relaxed mb-5 text-gray-600">
             Post-ironic portland shabby chic echo park, banjo fashion axe
           </p>
@@ -74,7 +69,7 @@ const ContactsPage = () => {
             />
           </div>
           <div className="relative mb-4">
-            <label for="message" className="leading-7 text-sm text-gray-600">
+            <label htmlFor="message" className="leading-7 text-sm text-gray-600">
               Message
             </label>
             <textarea
@@ -87,8 +82,7 @@ const ContactsPage = () => {
             Button
           </button>
           <p className="text-xs text-gray-500 mt-3">
-            Chicharrones blog helvetica normcore iceland tousled brook viral
-            artisan.
+            Chicharrones blog helvetica normcore iceland tousled brook viral artisan.
           </p>
         </div>
       </div>
