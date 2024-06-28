e09c3ba5b3d757e9908109ea7bb61587ec3c4c3d
svkhrobbeck <svkhrobbeck@gmail.com>
2024-06-28 18:14:09 +0500

    refactor: :recycle: refactor next config

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,24 +1,16 @@
 import { INavLink } from "@/interfaces";
 
 export const navLinks: INavLink[] = [
-  {
-    route: "/",
-    text: "Home",
-  },
-  {
-    route: "/contacts",
-    text: "Contacts",
-  },
-  {
-    route: "/products",
-    text: "All Products",
-  },
+  { route: "/", text: "Home" },
+  { route: "/contacts", text: "Contacts" },
+  { route: "/products", text: "All Products" },
 ];
 
 export const API_BASE_URL = "https://fakestoreapi.com";
+
 export const API_ENDPOINTS = {
-  PRODUCTS: "/products",
   PRODUCT: "/products/",
-  CATEGORIES: "/products/categories",
+  PRODUCTS: "/products",
   CATEGORY: "/products/category/",
+  CATEGORIES: "/products/categories",
 };
